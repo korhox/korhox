3c1df11e163451f9d88df5019a3cc728da602ae0
korhox <juuso.korhonen@bittivirta.fi>
2023-10-13 14:12:43 +0300

    feat: :lipstick: Add logos and modularize CSS

--- app/page/cv/page.js
+++ app/page/cv/page.js
@@ -1,7 +1,8 @@
 "use client";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import style from "../style/page.scss";
+import "../style/page.scss";
+import style from "../style/page.module.scss";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -10,22 +11,23 @@ library.add(fab, fas)
 
 import resume from "./resume.js";
 import moment from "moment";
+import Image from "next/image";
 
 
 const Button = ({ icon, text, onClick, href }) => {
     return href ?
-        <a href={href} className="cursor-pointer bg-white/70 flex items-center rounded-full">
-            <div className="bg-primary text-white w-10 h-10 rounded-full flex items-center justify-center" >
+        <a href={href} className={style.button}>
+            <div className={style.icon_holder} >
                 <FontAwesomeIcon icon={icon} />
             </div >
-            <span className="ps-3 pe-5 py-2 font-bold text-primary">{text}</span>
+            <span className={style.text}>{text}</span>
         </a >
         :
-        <button onClick={onClick} className="cursor-pointer bg-white/70 flex items-center rounded-full">
-            <div className="bg-primary text-white w-10 h-10 rounded-full flex items-center justify-center">
+        <button onClick={onClick} className={style.button}>
+            <div className={style.icon_holder} >
                 <FontAwesomeIcon icon={icon} />
             </div>
-            <span className="ps-3 pe-5 py-2 font-bold text-primary">{text}</span>
+            <span className={style.text}>{text}</span>
         </button>
 }
 
@@ -33,8 +35,8 @@ const Button = ({ icon, text, onClick, href }) => {
 const ProfileIcon = ({ network, username, link, icon }) => {
     icon = icon ? icon : ["fab", String(network).toLocaleLowerCase()]
     return (
-        <div className="flex items-center gap-2">
-            <FontAwesomeIcon icon={icon} className="text-primary" fixedWidth title={network} />
+        <div className={style.item}>
+            <FontAwesomeIcon icon={icon} className={style.icon} fixedWidth title={network} />
             <a href={link} target="_blank">{username}</a>
         </div>
     )
@@ -65,106 +67,136 @@ export default function Page() {
 
     return (
         <>
-            <div className="w-[210mm] m-4 mt-10 flex justify-between print:hidden">
+            <div className={style.page_controls}>
                 <div>
                     <h1>CV - {resume.basics.name}</h1>
                 </div>
-                <div className="flex gap-4">
+                <div className={style.buttons}>
                     <Button icon={["fas", "print"]} text="Print or Download" onClick={(e) => window.print()} />
                     <Button icon={["fas", "inbox-in"]} text="Download JSON Resume" href="/api/download/resume" />
                 </div>
             </div>
-            <main className="m-4 mt-0 h-[297mm] w-[210mm] overflow-hidden rounded-xl bg-white p-8 shadow-xl flex gap-6 print:m-0 print:p-0 print:h-screen print:w-screen print:rounded-none print:shadow-none">
-                <aside className="w-[30%] bg-slate-100 p-4 rounded-xl">
-                    <img src="https://avatars.githubusercontent.com/u/10478812" className="w-full rounded-full border-2 border-slate-400 mb-4" />
-                    {resume.basics.name && <h1>{resume.basics.name}</h1>}
-                    {resume.basics.label && <p className="opacity-60 mb-4">{resume.basics.label}</p>}
-                    {resume.basics.summary && <p className="text-sm">{resume.basics.summary}</p>}
-                    <div className="my-4 flex flex-col gap-2">
-                        {resume.basics.email && (
-                            <ProfileIcon network="Email" icon="envelope" username={resume.basics.email} url={`mailto:${resume.basics.email}`} />
-                        )}
-                        {resume.basics.url && (
-                            <ProfileIcon network="Website" icon="link" username={resume.basics.url} url={resume.basics.url} />
-                        )}
-                        {resume.basics.profiles && resume.basics.profiles.map((profile, i) => (
-                            <ProfileIcon key={"profile-" + i} network={profile.network} icon={profile.icon} username={profile.username} link={profile.url} />
-                        ))}
+            <main className={style.page}>
+                <aside className={style.sidebar}>
+                    <div>
+                        <img src="https://avatars.githubusercontent.com/u/10478812" className={style.avatar} />
+                        {resume.basics.name && <h1>{resume.basics.name}</h1>}
+                        {resume.basics.label && <p className={style.label}>{resume.basics.label}</p>}
+                        {resume.basics.summary && <p className={style.summary}>{resume.basics.summary}</p>}
+                        <div className={style.profile}>
+                            {resume.basics.email && (
+                                <ProfileIcon network="Email" icon="envelope" username={resume.basics.email} url={`mailto:${resume.basics.email}`} />
+                            )}
+                            {resume.basics.url && (
+                                <ProfileIcon network="Website" icon="link" username={resume.basics.url} url={resume.basics.url} />
+                            )}
+                            {resume.basics.profiles && resume.basics.profiles.map((profile, i) => (
+                                <ProfileIcon key={"profile-" + i} network={profile.network} icon={profile.icon} username={profile.username} link={profile.url} />
+                            ))}
+                        </div>
+                        <div className="my-10">
+                            <h2>Lanuage Skills</h2>
+                            {resume.languages && resume.languages.map((language, i) => (
+                                <div key={"language-" + i} className="mb-2 p-3 bg-slate-200 rounded-lg flex">
+                                    <span className="text-sm font-semibold flex-1 pe-1">{language.language}</span>
+                                    <span className="text-sm font-normal">{language.fluency}</span>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="my-10">
+                            <h2>Certifcates</h2>
+                            {resume.certificates && resume.certificates.map((certificate, i) => (
+                                <div key={"certificate-" + i} className="mb-2 p-3 bg-slate-200 rounded-lg flex items-center">
+                                    <h3 className="text-sm font-normal flex-1 pe-1">{certificate.shortName ? certificate.shortName : certificate.name}</h3>
+                                    {certificate.url && <a href={certificate.url} className="text-primary opacity-80 transition-all ease-in-out hover:scale-125 hover:opacity-100"><FontAwesomeIcon icon={["fas", "arrow-up-right-from-square"]} fixedWidth /></a>}
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                    <div className="my-10">
-                        <h2 className="ps-1 pb-1 text-lg">Lanuage Skills</h2>
-                        {resume.languages && resume.languages.map((language, i) => (
-                            <div key={"language-" + i} className="mb-2 p-3 bg-slate-200 rounded-lg flex">
-                                <span className="text-sm font-semibold flex-1 pe-1">{language.language}</span>
-                                <span className="text-sm font-normal">{language.fluency}</span>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="my-10">
-                        <h2 className="ps-1 pb-1 text-lg">Certifcates</h2>
-                        {resume.certificates && resume.certificates.map((certificate, i) => (
-                            <div key={"certificate-" + i} className="mb-2 p-3 bg-slate-200 rounded-lg flex items-center">
-                                <h3 className="text-sm font-normal flex-1 pe-1">{certificate.shortName ? certificate.shortName : certificate.name}</h3>
-                                {certificate.url && <a href={certificate.url} className="text-primary opacity-80 transition-all ease-in-out hover:scale-125 hover:opacity-100"><FontAwesomeIcon icon={["fas", "arrow-up-right-from-square"]} fixedWidth /></a>}
-                            </div>
-                        ))}
+                    <div>
+                        Online version of this CV is available at <a href="https://korho.fi/cv" className="text-primary">korho.fi/cv</a>
                     </div>
                 </aside>
-                <article className="w-[70%]">
+                <article className={style.article}>
                     <h2 className="ps-1 pb-1">Skills</h2>
-                    <div className="p-3 bg-slate-100 rounded-lg">
+                    <div className="p-3 bg-slate-100 rounded-lg flex flex-col gap-3">
                         {resume.skills && resume.skills.map((skill, i) => (
-                            <div key={"skills-" + i} className="mb-2">
-                                <h3>{skill.name}</h3>
-                                <p className="opacity-60">{skill.summary}</p>
+                            <div key={"skills-" + i}>
+                                <h3 className="text-xs">{skill.name}</h3>
                                 <div className="flex flex-wrap gap-1 mt-1">
                                     {skill.keywords && skill.keywords.map((keyword, j) => (
-                                        <span key={"skill-" + j} className="bg-primary text-white px-2 py-[0.16rem] rounded-full inline-block text-xs">{keyword}</span>
+                                        <span key={"skill-" + j} className={"bg-primary text-white px-2 py-[0.16rem] rounded-full inline-block text-xs"}>{keyword}</span>
                                     ))}
                                 </div>
                             </div>
                         ))}
                     </div>
                     <h2 className="ps-1 pb-1">Work Experience</h2>
-                    {resume.work && resume.work.map((work, i) => (
-                        <div key={"work-" + i} className="mb-2 p-3 bg-slate-100 rounded-lg">
-                            <h3>{work.position}</h3>
-                            <p className="opacity-60 text-xs flex gap-1">
-                                <span>
-                                    {work.name}
-                                </span>
-                                <span className="opacity-50">•</span>
-                                <span>
-                                    {moment(work.startDate).format("MMM YYYY")} » {work.endDate ? moment(work.endDate).format("MMM YYYY") : "Present"}
-                                </span>
-                                <span className="opacity-50">•</span>
-                                <span>
-                                    {/* Add two days to calculate months correctly */}
-                                    {formatDuration(moment(work.endDate).add("2", "days").diff(moment(work.startDate)))}
-                                </span></p>
-                            <p>{work.summary}</p>
-                        </div>
-                    ))}
+                    <div className="p-3 bg-slate-100 rounded-lg flex flex-col gap-3">
+                        {resume.work && resume.work.map((work, i) => (
+                            <div key={"work-" + i} className="flex gap-3 [&:not(:last-child)]:border-b border-slate-200 [&:not(:last-child)]:pb-3">
+                                <div className="min-w-[34px]">
+                                    {work.logo ?
+                                        <Image src={work.logo} alt={work.name} width={34} height={34} className="rounded-lg" />
+                                        :
+                                        <div className="bg-slate-200 text-primary rounded-lg w-[34px] h-[34px] flex items-center justify-center">
+                                            <FontAwesomeIcon icon={["fas", "briefcase"]} fixedWidth />
+                                        </div>
+                                    }
+                                </div>
+                                <div>
+                                    <h3>{work.position}</h3>
+                                    <p className="opacity-60 text-xs flex gap-1">
+                                        <span>
+                                            {work.name}
+                                        </span>
+                                        <span className="opacity-50">•</span>
+                                        <span>
+                                            {moment(work.startDate).format("MMM YYYY")} » {work.endDate ? moment(work.endDate).format("MMM YYYY") : "Present"}
+                                        </span>
+                                        <span className="opacity-50">•</span>
+                                        <span>
+                                            {/* Add two days to calculate months correctly */}
+                                            {formatDuration(moment(work.endDate).add("2", "days").diff(moment(work.startDate)))}
+                                        </span></p>
+                                    <p>{work.summary}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                     <h2 className="ps-1 pb-1">Education</h2>
-                    {resume.education && resume.education.map((education, i) => (
-                        <div key={"education-" + i} className="mb-2 p-3 bg-slate-100 rounded-lg">
-                            <h3>{education.institution}</h3>
-                            <p className="opacity-60 text-xs flex gap-1">
-                                <span>
-                                    {education.studyType}
-                                </span>
-                                <span className="opacity-50">•</span>
-                                <span>
-                                    {moment(education.startDate).format("MMM YYYY")} » {education.endDate ? moment(education.endDate).format("MMM YYYY") : "Present"}
-                                </span>
-                                <span className="opacity-50">•</span>
-                                <span>
-                                    {/* Add two days to calculate months correctly */}
-                                    {formatDuration(moment(education.endDate).add("2", "days").diff(moment(education.startDate)))}
-                                </span></p>
-                            <p>{education.summary}</p>
-                        </div>
-                    ))}
+                    <div className="p-3 bg-slate-100 rounded-lg flex flex-col gap-3">
+                        {resume.education && resume.education.map((education, i) => (
+                            <div key={"education-" + i} className="flex gap-3 [&:not(:last-child)]:border-b border-slate-200 [&:not(:last-child)]:pb-3">
+                                <div className="min-w-[34px]">
+                                    {education.logo ?
+                                        <Image src={education.logo} alt={education.name} width={34} height={34} className="rounded-lg" />
+                                        :
+                                        <div className="bg-slate-200 text-primary rounded-lg w-[34px] h-[34px] flex items-center justify-center">
+                                            <FontAwesomeIcon icon={["fas", "graduation-cap"]} fixedWidth />
+                                        </div>
+                                    }
+                                </div>
+                                <div>
+                                    <h3>{education.institution}</h3>
+                                    <p className="opacity-60 text-xs flex gap-1">
+                                        <span>
+                                            {education.studyType}
+                                        </span>
+                                        <span className="opacity-50">•</span>
+                                        <span>
+                                            {moment(education.startDate).format("MMM YYYY")} » {education.endDate ? moment(education.endDate).format("MMM YYYY") : "Present"}
+                                        </span>
+                                        <span className="opacity-50">•</span>
+                                        <span>
+                                            {/* Add two days to calculate months correctly */}
+                                            {formatDuration(moment(education.endDate).add("2", "days").diff(moment(education.startDate)))}
+                                        </span></p>
+                                    <p>{education.summary}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </article>
             </main >
             <p className="text-center print:hidden mt-4">
