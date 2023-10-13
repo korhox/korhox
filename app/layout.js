179738a5b66b926976663945be9184b5701020f2
korhox <juuso.korhonen@bittivirta.fi>
2023-10-13 22:47:30 +0300

    feat: :sparkles: Change font

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import './style/globals.scss'
-import { Ubuntu } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
-const ubuntu = Ubuntu({ subsets: ['latin'], weight: ['300', '400', '500', '700'] })
+const inter = Inter({ subsets: ['latin'], weight: ['300', '400', '500', '700'] })
 
 export const metadata = {
   title: 'Juuso "korho" Korhonen',
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
